b14ddcd9c5c1644b14e60f2a2d22fef321aa1bf1
drumnation <davidmieloch@gmail.com>
2017-04-02 14:02:04 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,6 @@
 def count_elements(array)
-  # code goes here
+  count = array.each_with_object(Hash.new(0)) {|ele, count| count[ele] += 1}
+  # saves result of initializing a new hash + iterating through array to count
+  # each element becomes a key in the hash count
+  # each key value is equal to the number of times that element is found in the array
 end
- 
